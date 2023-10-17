9b92c313d99e71522e82deba221d66e0a6dcc0df
Abdisemed Abdullahi <abdisemed20@gmail.com>
2023-10-17 11:33:53 +0300

    completed assignmnent

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if (ride <= 400) {
+    return "This one is on me!"
+  }
+  else if (ride > 2000 && ride <2500) {
+    return "I will gladly take your thirty bucks."
+  }
+  else if (ride >= 2500) {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  const cityMessage = city === "NYC" ? "Ok, sounds good." : "No go."
+  return cityMessage;
 }
+ternaryCheckCity('NYC')
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous':
+      return "Thank you so much."
+    case 'not as generous':
+      return "Thank you."
+    default :
+    return "Bye."
+  }  // Write your code here!
 }
+switchOnCharmFromTip("Thank you for everything");
